881da62cbe31e6def0f6883f4fe28e7455fc688c
Edoardo Fratarcangeli <edoardo.fratarcangeli0@gmail.com>
2025-10-05 12:47:37 +0200

    Added base files and intro

---
+++ Settings/settings.py
@@ -0,0 +1,4 @@
+# User settings and costants
+class User:
+    name = "Edoardo"
+    surname = "Fratarcangeli"
---
+++ main.py
@@ -0,0 +1,5 @@
+from Utility.printme import *
+
+if __name__ == "__main__":
+    intro()
+
